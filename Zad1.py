d2962389c4ad198310216f5a99285b8a185a2355
Toni <tonikovac.os@gmail.com>
2025-07-03 02:33:45 +0200

    New

--- Zad1.py
+++
@@ -1,6 +0,0 @@
-def total_euro(radniSati, satnica):
-    print("Korisnik je zaradio:", radniSati * satnica, " eura.")
-
-radniSati = float(input("Radni sati: "))
-satnica = float(input("eura/h: "))
-total_euro(radniSati, satnica)
